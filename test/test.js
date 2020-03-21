66395b52b0d1a8d30403559da529b94f5117b42c
Alwin Blok <alwinb@users.noreply.github.com>
2020-03-21 12:46:14 +0100

    README, example, towards 0.9.0

--- test/test.js
+++ test/test.js
@@ -1,19 +1,22 @@
 const tinyhtml = require ('../lib')
+const util = require ('util')
 const log = console.log.bind (console)
 
 var sample = '<span>Hello, world</span>'
+var sample = '<span>&lt&#x1F33F; Hello, world! &#129421;&gt</span attr="&lt=&lt">'
+
 var stream = tinyhtml.chunks (sample)
 
-log (stream.state)
+//log (stream.state)
 for (let chunk of stream) {
   log (chunk)
-  log (stream.state)
+  //log (stream.state)
 }
 
 
-var stream = tinyhtml.TokenBuilder.build (tinyhtml.chunks (sample))
-log (stream)
+var stream = tinyhtml.tags (sample)
+//log (stream.state)
 for (let token of stream) {
-  log (token)
-  log (stream)
+  log (util.inspect (token))
+  //log (stream.state)
 }
